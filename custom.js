caa47a4de04faa267b451fc0d3c91c0687503ae6
DippinDrip <farnoodfamilmomen@gmail.com>
2025-05-28 19:26:28 -0400

    a

--- custom.js
+++ custom.js
@@ -1,13 +1,16 @@
 function toggleMenu() {
-  document.getElementById("menuShell").classList.toggle("showNav");
-  document.getElementById("menuBtn").classList.toggle("active");
-}
-
-window.onscroll = function () {
-  var nav = document.getElementById("customNav");
-  if (document.body.scrollTop > 350 || document.documentElement.scrollTop > 350) {
-    nav.classList.add("navScrollDark");
-  } else {
-    nav.classList.remove("navScrollDark");
+    document.getElementById("menuShell").classList.toggle("showNav");
+    document.getElementById("menuBtn").classList.toggle("active");
   }
-};
+  
+
+
+  window.onscroll = function () {
+    var nav = document.getElementById("customNav");
+    if (document.body.scrollTop > 350 || document.documentElement.scrollTop > 350) {
+      nav.classList.add("navScrollDark");
+    } else {
+      nav.classList.remove("navScrollDark");
+    }
+  };
+  
